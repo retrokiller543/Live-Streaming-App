155a4e6504c3c8694387fe11d1efc4a7c6797948
retrokiller543 <emil.schutt@gmail.com>
2022-08-30 15:57:47 +0200

    Added mongoDB and mongo-express

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,11 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello there</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <video id="videoPlayer" width="50%" controls muted="muted" autoplay>
+        <source src="http://localhost:4000/video" type="video/mp4" />
+      </video>
     </div>
   );
 }
