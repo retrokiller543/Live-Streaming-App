31920a696ba16650524146aec68280754078ce30
retrokiller543 <emil.schutt@gmail.com>
2022-08-30 23:20:56 +0200

    Attempting to make the NMS work as the MS

--- app/src/App.js
+++ app/src/App.js
@@ -3,9 +3,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <video id="videoPlayer" width="50%" controls muted="muted" autoplay>
-        <source src="http://localhost:4000/video" type="video/mp4" />
-      </video>
+      <video
+        id="videoPlayer"
+        src="https://localhost:8443/live/emil.flv"
+        width="50%"
+        controls
+        muted="muted"
+        autoPlay
+      ></video>
     </div>
   );
 }
