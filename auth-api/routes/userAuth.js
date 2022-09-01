e65c8c394feeaadc38fc5879d0f0e8b1cbbf73a8
retrokiller543 <emil.schutt@gmail.com>
2022-09-01 23:34:02 +0200

    The auth-api is now mostly complete

--- auth-api/routes/userAuth.js
+++ auth-api/routes/userAuth.js
@@ -79,7 +79,7 @@ router.post("/register", async (req, res) => {
   });
   try {
     const savedUser = await user.save();
-    res.send({ user: uuid });
+    res.send(savedUser.uuid);
   } catch (err) {
     res.status(400).send(err);
   }
