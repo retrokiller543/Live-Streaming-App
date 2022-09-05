01df8f16edfea3b593e28cc8b6df691bcdd2bbe3
Retrokiller543 <56911974+Retrokiller543@users.noreply.github.com>
2022-09-05 11:50:42 +0200

    Added register page that dosent export

--- app/src/App.js
+++ app/src/App.js
@@ -2,25 +2,21 @@ import { Routes, Route } from "react-router-dom";
 
 import { Login, UserProfile, Video } from "./pages/index";
 
+import { Register } from "./pages/Register";
+
 // import Flvplayer from "./components/FlvPlayer";
 
 import "./App.css";
 
-// const checkLoginStatus = () => {};
-
-// const checkToken = (to, from, next) => {
-//   console.log(to);
-//   console.log(from);
-//   console.log(next);
-//   next();
-// };
-
 const App = () => {
-  <Routes>
-    <Route path="/" element={<Video />} />
-    <Route path="/users/user/profile" element={<UserProfile />} />
-    <Route path="/login" element={<Login />} />
-  </Routes>;
+  <main>
+    <Register />
+    <Routes>
+      <Route path="/" element={<Video />} />
+      <Route path="/users/user/profile" element={<UserProfile />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </main>;
 };
 
 export default App;
--- app/src/contexts/ContextProvider.js
+++ app/src/contexts/ContextProvider.js
@@ -2,15 +2,52 @@ import React, { createContext, useContext, useState } from "react";
 
 const StateContext = createContext();
 
-// const initialState = {
-//   chat: false,
-//   cart: false,
-//   userProfile: false,
-//   notification: false,
-// };
-
 export const ContextProvider = ({ children }) => {
-  return <StateContext.Provider value={{}}>{children}</StateContext.Provider>;
+  const [user, setUser] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [userFocus, setUserFocus] = useState(false);
+
+  const [pwd, setPwd] = useState("");
+  const [validPwd, setValidPwd] = useState(false);
+  const [pwdFocus, setPwdFocus] = useState(false);
+
+  const [matchPwd, setMatchPwd] = useState("");
+  const [validMatch, setValidMatch] = useState(false);
+  const [matchFocus, setMatchFocus] = useState(false);
+
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  return (
+    <StateContext.Provider
+      value={{
+        user,
+        setUser,
+        validName,
+        setValidName,
+        userFocus,
+        setUserFocus,
+        pwd,
+        setPwd,
+        validPwd,
+        setValidPwd,
+        pwdFocus,
+        setPwdFocus,
+        matchPwd,
+        setMatchPwd,
+        validMatch,
+        setValidMatch,
+        matchFocus,
+        setMatchFocus,
+        errMsg,
+        setErrMsg,
+        success,
+        setSuccess,
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
 };
 
 export const useStateContext = () => useContext(StateContext);
