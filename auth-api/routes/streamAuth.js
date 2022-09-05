d9e95221081a61b48c3613851a0786c915c54974
Kasper Thörnsten <ghosthookcc@horatio.localdomain>
2022-09-05 12:03:44 +0200

    Auth now works for auth-api -- media-server

--- auth-api/routes/streamAuth.js
+++ auth-api/routes/streamAuth.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const User = require("../model/User");
 
 router.post("/auth", async (req, res) => {
   try 
@@ -9,8 +10,14 @@ router.post("/auth", async (req, res) => {
       user: req.body.user,
       allow_access: false,
     };
+
+    const foundUser = await User.findOne({ $and:
+	    					[{ username: { $eq: req.body.user } }, 
+						 { uuid: { $eq: req.body.uuid } }] 
+    					}); 
+    if (foundUser) auth_decision.allow_access = true; 
     
-    res.status(200).send(auth_decision)
+    res.status(200).send(auth_decision);
   } catch (error) { 
     console.error("Error: " + error); 
     res.status(400).send(error); 
