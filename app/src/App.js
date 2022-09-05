d2b291a2555a173ba7c40acf229e0a3feda21da0
retrokiller543 <emil.schutt@gmail.com>
2022-09-05 14:46:50 +0200

    Made the Register page import

--- app/src/App.js
+++ app/src/App.js
@@ -1,22 +1,20 @@
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
 
-import { Login, UserProfile, Video } from "./pages/index";
-
-import { Register } from "./pages/Register";
+import { Register } from "./pages/index";
 
 // import Flvplayer from "./components/FlvPlayer";
 
 import "./App.css";
 
 const App = () => {
-  <main>
+  <div>
     <Register />
-    <Routes>
+    {/* <Routes>
       <Route path="/" element={<Video />} />
       <Route path="/users/user/profile" element={<UserProfile />} />
       <Route path="/login" element={<Login />} />
-    </Routes>
-  </main>;
+    </Routes> */}
+  </div>;
 };
 
 export default App;
--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -3,3 +3,4 @@ export { default as UserProfile } from "./UserProfile";
 export { default as Video } from "./Video";
 export { default as Loading } from "./Loading";
 export { default as NotFound } from "./NotFound";
+export { default as Register } from "./Register";
