317318f49f68214e159b85b8782fd91316231e2e
retrokiller543 <emil.schutt@gmail.com>
2022-10-07 14:13:27 +0200

    added eslint and prettier-eslint

---
+++ app/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:prettier/recommended',
+	],
+	'overrides': [
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react',
+		'prettier'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'windows'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'prettier/prettier': 'error'
+	}
+};
